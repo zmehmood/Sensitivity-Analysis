f19641ef8270f8dfd3f265a037b3b54b6cb718f3
Zeeshan Mehmood <zeeshan.mehmood@sbs-dresden.de>
2018-10-11 15:00:29 +0200

    request replayed back 2

--- index.py
+++ index.py
@@ -5,11 +5,19 @@ import datetime
 
 
 def handler(event, context):
-    data = {
-        'event': str(event['body']),
-        'timestamp': datetime.datetime.utcnow().isoformat()
-        
-    }
+    if event is not None:
+        data = {
+            'event': str(event['body']),
+            'timestamp': datetime.datetime.utcnow().isoformat()
+            
+        }
+    else:
+        data = {
+            'event': str(event),
+            'timestamp': datetime.datetime.utcnow().isoformat()
+            
+        }
+    
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
