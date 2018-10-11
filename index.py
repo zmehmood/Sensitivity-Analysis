2c3b9944cf74b25010ae51c0f3d57e1de80dc52b
Zeeshan Mehmood <zeeshan.mehmood@sbs-dresden.de>
2018-10-11 11:31:37 +0200

    request replayed back

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
+# coding: utf-8
+
 import json
 import datetime
 
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'event': str(event),
         'timestamp': datetime.datetime.utcnow().isoformat()
+        'context':str(context)
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
